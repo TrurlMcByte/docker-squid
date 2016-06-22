6746b8fe8b00a1ec4e506fc1d1050361360403ab
Trurl McByte <trurl@mcbyte.net>
2016-06-22 22:45:46 +0300

    fix permissions

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-set -e
-test -d "`sed -n 's/^cache_dir\s\+[[:alnum:]]\+\s\+\([[:graph:]\/]\+\)\s.*/\1/p' /etc/squid/squid.conf | sed '1 q'`/00" || /usr/sbin/squid -z -F -N -S -f /etc/squid/squid.conf
+set -ex
+CACHE_DIR="`sed -n 's/^cache_dir\s\+[[:alnum:]]\+\s\+\([[:graph:]\/]\+\)\s.*/\1/p' /etc/squid/squid.conf | sed '1 q'`"
+chown squid.squid $CACHE_DIR
+test -d "${CACHE_DIR}/00" || /usr/sbin/squid -z -F -N -S -f /etc/squid/squid.conf
 
 if [ $# -eq 0 ]; then
     /usr/sbin/squid -F -S -N -f /etc/squid/squid.conf
